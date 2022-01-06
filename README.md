c321734eda264b1818c2d7212e123a9e1f9d18f5
ubinix-warun <warun@ubinix.com>
2022-01-06 15:03:48 +0700

    ADD: Blank Ext

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+```
+nvm use v16.10.0
+yarn add @types/chrome -D
+
+```
+
 ## Available Scripts
 
 In the project directory, you can run:
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
-import React from 'react';
+import React ,{ useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [url, setUrl] = useState<string>('');
+
+  /**
+   * Get current URL
+   */
+  useEffect(() => {
+      const queryInfo = {active: true, lastFocusedWindow: true};
+
+      chrome.tabs && chrome.tabs.query(queryInfo, tabs => {
+          const url = tabs[0].url || '';
+          setUrl(url);
+      });
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo"/>
+              <p>URL:</p>
+              <p>
+                  {url}
+              </p>
+          </header>
+      </div>
   );
 }
 
